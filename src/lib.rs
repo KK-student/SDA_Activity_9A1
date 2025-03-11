c9137ca8c1923b10357abf07902201e1abca0f37
Kseniya Kolokolkina <kseniya.kolokolkina@ontariotechu.net>
2025-03-11 19:46:00 -0400

    Made the reverse_string reverse the given original string, ignoring punctuation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,87 @@
-/*
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::char;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn reverse_string(original: &String) -> String {
+    let original_bytes = original.as_bytes();
+    let length: usize = original.len();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /*
+    let mut result: String = String::with_capacity(length);
+
+    {
+        let mut i: usize = 0;
+        while i < length {
+            result.push(' ');
+
+            i += 1;
+        }
     }
-}
-*/
 
-pub fn reverse_string(original: String) -> String {
-    let result: String = String::new();
+    {
+        let mut i: usize = 0;
+        while i < length {
+            let r_i = length - 1 - i;
+            //result.insert(r_i, original_bytes[i] as char);
+            //result.replace_range(r_i..r_i, &original_bytes[i].to_string()[0..0]);
+            result.chars().nth(r_i).unwrap() = original_bytes[i];
+
+            i += 1;
+        }
+    }
 
     return result;
+    */
+
+    let mut result_chars: Vec<u8> = Vec::with_capacity(length);
+
+    {
+        let mut i: usize = 0;
+        while i < length {
+            result_chars.push(' ' as u8);
+
+            i += 1;
+        }
+    }
+
+    {
+        let mut i: usize = 0;
+        while i < length {
+            let r_i = length - 1 - i;
+            result_chars[r_i] = original_bytes[i];
+
+            i += 1;
+        }
+    }
+
+    return String::from_utf8(result_chars).expect("Invalid UTF-8");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test] 
+    //#[test] 
     fn test1() {
+        //Test if there is a reverse_string method that accepts 1 String parameter and returns an
+        //empty string. 
         let original: String = "abcd".to_string(); //Could be any string. 
-        let result: String = reverse_string(original);
+        let result: String = reverse_string(&original);
         assert_eq!(result, String::new());
+    }
+
+    #[test]
+    fn test2() {
+        //Test if the reverse_string method returns a String with the same or greater capacity as the length of the given original String. 
+        let original: String = "abcdefg".to_string();
+        let result: String = reverse_string(&original);
+        assert!(result.capacity() >= original.len());
+    }
+
+    #[test]
+    fn test3() {
+        //Test if the reverse_string function returns the reversed version of "abcde".
+        let original: String = "abcde".to_string();
+        let result: String = reverse_string(&original);
+        assert_eq!(result, "edcba".to_string());
     }
 }
 
